6982bf110c4f8e70082a23f51b75e17a28e0be4c
grifth <2473713213@qq.com>
2018-12-26 03:10:04 +0800

    添加上传文件功能

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 var fs = require('fs');
 var express = require('express')
 var app = express()
+var qiniu = require('qiniu')
 // GET method route
-app.get('/', function (req, res) {
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials','true');
+    next();
+};
+
+app.use(allowCrossDomain);
+
+
+app.get('/uptoken', function (req, res) {
     res.setHeader('Access-Control-ALllow-Origin',"*")
 
-    var json = fs.readFileSync('./qiniukey.json')
+    var config = fs.readFileSync('./qiniu-key.json')
+    config = JSON.parse(config)
+
+
+
+    let {accessKey,secretKey} = config
+
+    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+    var options = {
+      scope: 'test',
+    };
+    var putPolicy = new qiniu.rs.PutPolicy(options);
+    var uploadToken=putPolicy.uploadToken(mac);
+
 
     res.write(`
       {
-        "uptoken":${json}
+        "uptoken":"${uploadToken}"
       }
       `)
     res.end()
